2a349526c3b3dfd299d75d78fdb1a9702c03f7f2
MHzIT <mhzit@Mikaels-MacBook-Pro.local>
2022-12-09 13:27:03 +0100

    Initial commit

---
+++ src/test/java/mhz/labs/restjpa/RestjpaApplicationTests.java
@@ -0,0 +1,13 @@
+package mhz.labs.restjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestjpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
